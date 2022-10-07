2a486d573ef08cb359fed9f318933b8caa0d7ddd
IsraMejia <ihmaisrael@gmail.com>
2022-10-06 22:54:51 -0500

    Cat objects and older cat algorithm

---
+++ OOP/cat.py
@@ -0,0 +1,30 @@
+#Given the below class:
+class Cat:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# 1 Instantiate the Cat object with 3 cats
+gato1 = Cat('michi',10000)
+gato2 = Cat('pixi',1300)
+gato3 = Cat('batu',92999)
+
+# 2 Create a function that finds the oldest cat
+cats_list = [gato1,gato2, gato3]
+
+def older_cat(cats):
+    oldest = 0 
+    print('\nIs time to know how is the oldest cat:')
+    for i in range(len(cats)):
+        if cats[oldest].age < cats[i].age: 
+            oldest = i
+            print(f'there are a new older cat: {cats[oldest].name}')
+        else: 
+            print(f'currently {cats[oldest].name} is the oldest cat')
+    print(f'\n\tFinally the oldest cat is {cats[oldest].name} with {cats[oldest].name} years old')
+
+
+older_cat(cats_list)
+
--- OOP/oop.py
+++ OOP/oop.py
@@ -12,18 +12,35 @@ print(f"I created a object  : { type(Rex) }")
 """
 
 class PlayerCharacter :
-    def __init__(self, name ) :
-        self.name = name
+    #Class Object atributte
+    membership = True
+
+    def __init__(self, name = 'default_name' , age = 0) :
+        #if (self.membership):
+        #if (PlayerCharacter.membership): #if the instance has membership
+        if (age > 18):
+            self.name = name #Instance Attributes
+            self.age =age
     
-    def run (self):
+    def run (self): #Method
         print('run')
 
-player1 = PlayerCharacter('Isra')
-player2 = PlayerCharacter('Pam')
+    def shout(self):
+        print(f'My name es {self.name}')
+    
+    def phrase(self, text):
+        print(f'My name is {self.name}, and I want say: {text}')
 
-print(f'Player 1 created, name: {player1.name}')
-print(f'Memory location: {player1}')
+
+player1 = PlayerCharacter('Isra', 24)
+player2 = PlayerCharacter('Pam', 22)
+
+print(f'\nPlayer 1 created, name: {player1.name}')
+print(f'Memory location: {player1} \n')
+player1.run()
 
 print(f'\n\nPlayer 2 created, name: {player2.name}')
 print(f'Memory location: {player2}')
+player2.shout()
 
+player1.phrase('Estoy mamadisimo alv jaja')
