761998a720af9412cd2e0353c3561e113d080aad
IsraMejia <ihmaisrael@gmail.com>
2023-11-29 21:50:02 -0600

    kmeans inicio

---
+++ othersxd/kmeans.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class KMeansClustering:
+
+    def __init__(self, k=3) :
+        self.k = k
+        self.centroids = None
+
+    
+    def  fit(self, X, max_iterations = 200):
+        self.centroids = np.random.uniform(
+                            np.amin(X, axis=0),         #X es la variable independiente o Caracteristica
+                            np.amax(X, axis = 0), 
+                            size= (self.k, X.shape[1]) 
+                        )
